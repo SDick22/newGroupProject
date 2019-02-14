4184d09f167d733781740a06d7bfba03aaabab63
SAD22 <SAD22@FSTU603036.pti.edu>
2019-02-14 11:52:46 -0500

    Added an addition to the code

--- src/groupproject1/GroupProject1.java
+++ src/groupproject1/GroupProject1.java
@@ -17,6 +17,7 @@ public class GroupProject1 {
     public static void main(String[] args) 
     {
         System.out.println("Hello World");
+        System.out.println("Sam's addition to the code");
     }
     
 }
